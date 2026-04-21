c8b717d4b9eec7f4fdfbd63eab1ee905b378ceb8
Leandra Pires <leandrapiress15@gmail.com>
2026-04-21 12:38:22 -0300

    Refatorando a aplicação para utilizar templates

--- lists/tests.py
+++ lists/tests.py
@@ -10,10 +10,11 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
+
+        self.assertTrue(html.strip().startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
