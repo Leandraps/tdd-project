6a9506421f23b74a544338ce236e1651a6168607
Leandra Pires <leandrapiress15@gmail.com>
2026-04-21 13:52:04 -0300

    Processando Requisição POST

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse('OK')
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
+    return render(request, 'lists/home.html')
