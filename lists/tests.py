658766a38c8e1598b4175c982c3b3586a184e394
Leandra Pires <leandrapiress15@gmail.com>
2026-04-22 21:22:12 -0300

    Apresentar Múltiplos Itens na Listagem

--- lists/tests.py
+++ lists/tests.py
@@ -2,24 +2,35 @@ from django.test import TestCase
 from lists.models import Item
 import os
 class HomePageTest(TestCase):
-    
 
-    def test_uses_home_template(self):
+
+    ...
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+...
+
+def test_uses_home_template(self):
         response = self.client.get('/')   
         self.assertTemplateUsed(response, 'lists/home.html')
-    def test_can_save_a_POST_request(self):
+def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-    def test_redirects_after_POST(self):
+def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
-    def test_saving_and_retrieving_items(self):
+def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'O primeiro item'
         first_item.save()
@@ -35,6 +46,6 @@ class HomePageTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'O primeiro item')
         self.assertEqual(second_saved_item.text, 'O segundo item')
-    def test_only_saves_items_when_necessary(self):
+def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
